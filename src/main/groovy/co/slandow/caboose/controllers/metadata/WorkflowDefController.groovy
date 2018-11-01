ecb2af7296f9cf71d5bf9bb2bf8f6022a74a405e
Steven Landow <greenpipestudios@gmail.com>
2018-11-01 13:44:35 -0500

    expose workflow def api

---
+++ src/main/groovy/co/slandow/caboose/controllers/metadata/WorkflowDefController.groovy
@@ -0,0 +1,34 @@
+package co.slandow.caboose.controllers.metadata
+
+import co.slandow.caboose.model.WorkflowDef
+import co.slandow.caboose.service.metadata.WorkflowDefService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/metadata/workflows")
+class WorkflowDefController {
+
+    @Autowired
+    private WorkflowDefService workflowDefService
+
+    @GetMapping
+    List<WorkflowDef> getWorkflowDefinitions(
+            @RequestParam(value = "page", defaultValue = "-1") int page,
+            @RequestParam(value = "size", defaultValue = "-1") int size) {
+        return page > -1 && size > -1 ? workflowDefService.getWorkflowDefs(page, size)
+                : workflowDefService.getWorkflowDefs()
+    }
+
+    @GetMapping("/{name}")
+    WorkflowDef getWorkflowDefinition(@PathVariable String name) {
+        return workflowDefService.getWorkflowDef(name)
+    }
+
+    @PutMapping
+    void createWorkflowDefinition(@RequestBody WorkflowDef workflowDef) {
+        workflowDefService.saveWorkflowDef(workflowDef)
+    }
+
+
+}
