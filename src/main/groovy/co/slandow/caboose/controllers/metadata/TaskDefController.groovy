e46b2e01f1f5920275a19ce088928289c795d122
Steven Landow <greenpipestudios@gmail.com>
2018-11-01 14:21:02 -0500

    expose taskdefs metadata api

---
+++ src/main/groovy/co/slandow/caboose/controllers/metadata/TaskDefController.groovy
@@ -0,0 +1,35 @@
+package co.slandow.caboose.controllers.metadata
+
+import co.slandow.caboose.model.metadata.TaskDef
+import co.slandow.caboose.model.metadata.WorkflowDef
+import co.slandow.caboose.service.metadata.TaskDefService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/metadata/taskdefs")
+class TaskDefController {
+
+    @Autowired
+    private TaskDefService taskDefService
+
+    @GetMapping
+    List<TaskDef> getWorkflowDefinitions(
+            @RequestParam(value = "page", defaultValue = "-1") int page,
+            @RequestParam(value = "size", defaultValue = "-1") int size) {
+        return page > -1 && size > -1 ? taskDefService.getTaskDefs(page, size)
+                : taskDefService.getTaskDefs()
+    }
+
+    @GetMapping("/{name}")
+    TaskDef getWorkflowDefinition(@PathVariable String name) {
+        return taskDefService.getTaskDef(name)
+    }
+
+    @PutMapping
+    void createWorkflowDefinition(@RequestBody TaskDef taskDef) {
+        taskDefService.saveTaskDef(taskDef)
+    }
+
+
+}
--- src/main/groovy/co/slandow/caboose/controllers/metadata/WorkflowDefController.groovy
+++ src/main/groovy/co/slandow/caboose/controllers/metadata/WorkflowDefController.groovy
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/metadata/workflows")
+@RequestMapping("/api/metadata/workflow")
 class WorkflowDefController {
 
     @Autowired
